2477cffb7c108ab0367e919851ab65414b8918af
hiddegrei <grei0005@hz.nl>
2022-12-19 19:17:17 +0100

    a

--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,10 @@ const cors = require("cors")({ origin: true });
 const app = express();
 app.use(cors);
 const admin = require("firebase-admin");
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 admin.initializeApp();
-// app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use(bodyParser.json());
-
-// const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
 
 exports.myFunction = functions.firestore.document("users/{userId}/calender/{yearId}/{monthId}/{dayId}").onUpdate((change, context) => {
   var today = new Date();
@@ -22,7 +19,7 @@ exports.myFunction = functions.firestore.document("users/{userId}/calender/{year
   console.log(change.after.data().goal, change.after.data().waterIntake);
   console.log(context.params.userId);
 
-  if (change.after.data().goal <= change.after.data().waterIntake) {
+  if (change.after.data().goal <= change.after.data().waterIntake&&!change.before.data().achieved) {
     admin
       .firestore()
       .collection("users")
@@ -35,34 +32,27 @@ exports.myFunction = functions.firestore.document("users/{userId}/calender/{year
       .catch((err) => {
         console.log(err);
       });
+       
+      fetch(`https://us-central1-ms-users.cloudfunctions.net/app/api/users/${context.params.userId}/streak`, {
+        method: "PUT", // or 'PUT',
+
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+      })
+        .then((res) => {
+
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     return 0;
   }
   return 1;
 });
 
-app.get("/", (req, res) => {
-  res.send(`
-    <!doctype html>
-    <head>
-      <title>Time</title>
-      <link rel="stylesheet" href="/style.css">
-     
-    </head>
-    <body>
-      <p>
-        <span id="bongs">api microservice waterintake</span></p>
-      
-    </body>
-  </html>`);
-});
-
-app.get("/api", (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
-  res.json({ bongs: "BONG ".repeat(hours) });
-});
-
-app.post("/api/users/:userId/create", (req, res) => {
+///api/users/:userId/create
+app.post("/api/users/:userId", (req, res) => {
   var today = new Date();
   let avaiTrue = false;
   for (let j = 0; j < 12; j++) {
@@ -99,51 +89,8 @@ app.post("/api/users/:userId/create", (req, res) => {
   }
 });
 
-app.get("/api/users/:userId/waterintake", (req, res) => {
-  var today = new Date();
-  var day = today.getDate();
-  var month = today.getMonth();
-  var year = today.getFullYear();
-  let dataR = [];
 
-  function resolveAfterEnd() {
-    return new Promise((resolve) => {
-      for (let i = 0; i < 7; i++) {
-        admin
-          .firestore()
-          .collection("users")
-          .doc(req.params.userId)
-          .collection("calender")
-          .doc(`${year}`)
-          .collection(`${month}`)
-          .doc(`${day + i}`)
-          .get()
-          .then((doc) => {
-            if (dataR.length == undefined) {
-              dataR[0] = doc.data();
-            } else {
-              dataR[dataR.length] = doc.data();
-            }
 
-            if (dataR.length === 7) {
-              console.log(dataR);
-              resolve("succes");
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    });
-  }
-  async function f1() {
-    const x = await resolveAfterEnd();
-  }
-  let result1 = f1().then(() => {
-    res.json({ data: dataR });
-    return dataR;
-  });
-});
 
 app.get("/api/users/:userId/waterintake/today", (req, res) => {
   var today = new Date();
@@ -171,7 +118,8 @@ var jsonParser = bodyParser.json();
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.post("/api/users/:userId/waterintake/today/update/", urlencodedParser, (req, res) => {
+///api/users/:userId/waterintake/today/update/
+app.put("/api/users/:userId/waterintake/today", urlencodedParser, (req, res) => {
   var today = new Date();
   var day = today.getDate();
   var month = today.getMonth();
@@ -203,6 +151,7 @@ app.get("/api/users/:userId/waterintake/lastweek", (req, res) => {
   var month = today.getMonth();
   var year = today.getFullYear();
   var dayOfWeek=today.getDay();
+  let days=["mon","tue","wed","thu","fri","sat","sun"]
 
   let data=[{index:0,data:[]},
   {index:1,data:[]},
@@ -211,45 +160,9 @@ app.get("/api/users/:userId/waterintake/lastweek", (req, res) => {
 {index:4,data:[]},
 {index:5,data:[]},
 {index:6,data:[]}]
-for(let i=dayOfWeek;i++;i<6){
-   admin
-    .firestore()
-    .collection("users")
-    .doc(req.params.userId)
-    .collection("calender")
-    .doc(`${year}`)
-    .collection(`${month}`)
-    .doc(`${day+i-dayOfWeek}`)
-    .get()
-    .then((doc) => {
-      // res.json({ data: doc.data() });
-      data[i].data=doc.data()
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+function f1(){
 
-}
-for (let i =1; i++; i <  dayOfWeek) {
-   admin
-    .firestore()
-    .collection("users")
-    .doc(req.params.userId)
-    .collection("calender")
-    .doc(`${year}`)
-    .collection(`${month}`)
-    .doc(`${day-dayOfweek+i-1}`)
-    .get()
-    .then((doc) => {
-      // res.json({ data: doc.data() });
-      data[i].data=doc.data()
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 
-}
-let sunIndex=dayOfWeek+dayOfweek%6
  admin
    .firestore()
    .collection("users")
@@ -257,18 +170,312 @@ let sunIndex=dayOfWeek+dayOfweek%6
    .collection("calender")
    .doc(`${year}`)
    .collection(`${month}`)
-   .doc(`${day + sunIndex}`)
+   .doc(`${day}`)
    .get()
    .then((doc) => {
      // res.json({ data: doc.data() });
-     data[day + sunIndex].data = doc.data();
-     res.json({ data: data });
+     
+       data[6].data = doc.data();
+
+     
+    if(day-1>=1){
+       f2()
+
+    }else{
+      month--
+     let newDate= new Date(year, month, 0).getDate();
+     day=newDate
+     f2()
+    }
+    
+     
    })
    .catch((err) => {
      console.log(err);
    });
+  }
+  function f2(){
+    
+   admin
+   .firestore()
+   .collection("users")
+   .doc(req.params.userId)
+   .collection("calender")
+   .doc(`${year}`)
+   .collection(`${month}`)
+   .doc(`${day- 1}`)
+   .get()
+   .then((doc) => {
+     // res.json({ data: doc.data() });
+     data[5].data = doc.data();
+     if (day - 2 >= 1) {
+       f3();
+     } else {
+       month--;
+       let newDate = new Date(year, month, 0).getDate();
+       day = newDate;
+       f3();
+     }
+     
+   })
+   .catch((err) => {
+     console.log(err);
+   });
+  }
+  function f3(){
+   admin
+   .firestore()
+   .collection("users")
+   .doc(req.params.userId)
+   .collection("calender")
+   .doc(`${year}`)
+   .collection(`${month}`)
+   .doc(`${day- 2}`)
+   .get()
+   .then((doc) => {
+     // res.json({ data: doc.data() });
+     data[4].data = doc.data();
+     if (day - 3 >= 1) {
+       f4();
+     } else {
+       month--;
+       let newDate = new Date(year, month, 0).getDate();
+       day = newDate;
+       f4();
+     }
+    
+   })
+   .catch((err) => {
+     console.log(err);
+   });
+  }
+  function f4(){
+   admin
+     .firestore()
+     .collection("users")
+     .doc(req.params.userId)
+     .collection("calender")
+     .doc(`${year}`)
+     .collection(`${month}`)
+     .doc(`${day - 3}`)
+     .get()
+     .then((doc) => {
+       // res.json({ data: doc.data() });
+       data[3].data = doc.data();
+       if(day-4>=1){
+       f5()
+
+    }else{
+      month--
+     let newDate= new Date(year, month, 0).getDate();
+     day=newDate
+     f5()
+    }
+       
+       
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+    }
+    function f5(){
+     admin
+       .firestore()
+       .collection("users")
+       .doc(req.params.userId)
+       .collection("calender")
+       .doc(`${year}`)
+       .collection(`${month}`)
+       .doc(`${day - 4}`)
+       .get()
+       .then((doc) => {
+         // res.json({ data: doc.data() });
+         data[2].data = doc.data();
+         if (day - 5 >= 1) {
+           f6();
+         } else {
+           month--;
+           let newDate = new Date(year, month, 0).getDate();
+           day = newDate;
+           f6();
+         }
+        
+         
+       })
+       .catch((err) => {
+         console.log(err);
+       });
+      }
+      function f6(){
+       admin
+         .firestore()
+         .collection("users")
+         .doc(req.params.userId)
+         .collection("calender")
+         .doc(`${year}`)
+         .collection(`${month}`)
+         .doc(`${day - 5}`)
+         .get()
+         .then((doc) => {
+           // res.json({ data: doc.data() });
+           data[1].data = doc.data();
+           if (day - 6 >= 1) {
+             f7();
+           } else {
+             month--;
+             let newDate = new Date(year, month, 0).getDate();
+             day = newDate;
+             f7();
+           }
+           
+          
+         })
+         .catch((err) => {
+           console.log(err);
+         });
+        }
+        function f7(){
+         admin
+   .firestore()
+   .collection("users")
+   .doc(req.params.userId)
+   .collection("calender")
+   .doc(`${year}`)
+   .collection(`${month}`)
+   .doc(`${day- 6}`)
+   .get()
+   .then((doc) => {
+     // res.json({ data: doc.data() });
+     data[0].data = doc.data();
+    
+     res.json({ data:data });
+    
+   })
+   .catch((err) => {
+     console.log(err);
+   });
+  }
+  f1()
+
 
  
 });
+app.get("/api/users/:userId/waterintake/lastmonth", (req, res) => {
+  var today = new Date();
+  var day = today.getDate();
+  var month = today.getMonth();
+  var year = today.getFullYear();
+  var dayOfWeek = today.getDay();
+  let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  let data=[];
+  let go=true
+ function resolveAfterEnd(yr,mon,d) {
+   return new Promise((resolve) => {
+     admin
+       .firestore()
+       .collection("users")
+       .doc(req.params.userId)
+       .collection("calender")
+       .doc(`${yr}`)
+       .collection(`${mon}`)
+       .doc(`${d }`)
+       .get()
+       .then((doc) => {
+         // res.json({ data: doc.data() });
+         if(doc.exists){
+data.push(doc.data()); 
+         }
+         
+          // console.log(doc.data());
+        //  res.json({ data: data });
+        resolve("succes")
+       })
+       .catch((err) => {
+         console.log(err);
+         resolve("non")
+       });
+   });
+ }
+ async function f1(yr,mon,d) {
+   const x = await resolveAfterEnd(yr,mon,d);
+ }
+ async function loopie() {   
+ for(let i=0;i<31;i++){
+  const result = await f1(year, month, day);
+  if (day - 1 >= 1) {
+    day--
+             
+           } else {
+             month--;
+             let newDate = new Date(year, month, 0).getDate();
+             day = newDate;
+            
+           }
+  
+  if(i===30){
+    console.log("done")
+    
+    return 0;
+   
+  }
 
-exports.app = functions.https.onRequest(app);
+ }
+}
+ loopie().then((doc)=>{
+  res.json({ data: data });
+
+ });
+console.log("last month")
+
+ 
+
+
+})
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: "2GB",
+};
+
+exports.app = functions.runWith(runtimeOpts).https.onRequest(app);
+// exports.scheduledFunctionEndDay = functions.pubsub
+//   .schedule("59 23 * * *")
+//   .timeZone("Europe/Paris")
+//   .onRun((context) => {
+//     var today = new Date();
+//     var day = today.getDate();
+//     var month = today.getMonth();
+//     var year = today.getFullYear();
+
+//     admin
+//       .firestore()
+//       .collection("users")
+//       .get()
+//       .forEach((doc) => {
+//         admin
+//           .firestore()
+//           .collection("users")
+//           .doc(doc.id)
+//           .collection("calender")
+//           .doc(`${year}`)
+//           .collection(`${month}`)
+//           .doc(`${day}`)
+//           .get()
+//           .then((doc) => {
+//             if (doc.data().goal <= doc.data().waterIntake) {
+//               admin
+//                 .firestore()
+//                 .collection("users")
+//                 .doc(doc.id)
+//                 .update({
+//                   streak: FieldValue.Increment(1),
+//                 });
+//             } else {
+//               admin.firestore().collection("users").doc(doc.id).update({
+//                 streak: 0,
+//               });
+//             }
+//           });
+
+//         admin.firestore().collection("users").doc(doc.id).collection("calender").doc(`${year}`).collection(`${month}`).doc(`${day}`).update({ available: false });
+//       });
+//   });
